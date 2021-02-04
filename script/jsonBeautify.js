c4eac933deec52ee0b5127fe711ba79890c001ae
Amr Abdallah <relatora@gmail.com>
2021-02-04 14:46:30 -0500

    products location update

--- script/jsonBeautify.js
+++ script/jsonBeautify.js
@@ -2,7 +2,7 @@ $(document).ready(function () {         // jQuery detects the state of readiness
     $("#json_beautify").jsonToWeb({     // Plugin invocation inside a container having this class
         // Compulsory settings
         imagesPath: 'images/strollers/',  //Image path must be specified and image names correspond to product/employee/...
-        jsonSource: 'http://12.42.204.30:7251/strollers.json',  // source can be a json array or a json address
+        jsonSource: './strollers.json',  // source can be a json array or a json address
         jsonParametersHTML: 'Stock number,Quantity on hand,Size,Color',   // No name included
         jsonParametersJSON: 'stock_number,QOH,size,color',  // length should be same as it's HTML counterpart for cohesion
         nameParameterJSON: 'name',       // will be the title of the box and image
